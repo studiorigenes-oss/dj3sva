5e0ab32fa4149297793cadeb4c36c32ceae31f0d
studiorigenes <studiorigenes@gmail.com>
2026-05-03 23:59:46 -0400

    Create footer.php

---
+++ dj3sva-minimal/footer.php
@@ -0,0 +1,21 @@
+<footer id="colophon" class="site-footer">
+    <div class="footer-container">
+        <div class="footer-top">
+            <h2 class="footer-logo">dj3sva</h2>
+            <div class="footer-social">
+                <a href="#">INSTAGRAM</a>
+                <a href="#">SPOTIFY</a>
+                <a href="#">YOUTUBE</a>
+                <a href="#">MIXCLOUD</a>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>&copy; <?php echo date('Y'); ?> dj3sva. ALL RIGHTS RESERVED.</p>
+            <p class="designer-tag">DESIGNED BY ANTIGRAVITY</p>
+        </div>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
